368ba5edc9282f87e1a7be2fddc43bbf39d902e4
tigran88 <free.mind.wow@gmail.com>
2018-03-02 13:54:59 +0400

    css style setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,41 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const PATHS = {
-    source: path.join(__dirname, 'src/js'),
+    source: path.join(__dirname, 'src'),
     build: path.join(__dirname, 'dist')
 };
 
 module.exports = {
-    entry: PATHS.source + '/index.js',
+    entry: {
+      app: [
+        PATHS.source + '/js/index.js',
+        PATHS.source + '/sass/main.scss'
+      ]
+    },
     output: {
-        path: PATHS.build,
-        filename: '[name].js'
-    }
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ['css-loader', 'sass-loader']
+          })
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ['css-loader']
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin("[name].css"),
+    ]
 };
